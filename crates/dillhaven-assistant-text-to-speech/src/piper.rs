c2b79f501b65965af13ecec522d68fbf616888c8
Dylan Ross <pdylanross@gmail.com>
2025-09-28 22:34:48 -0500

    fix: fmt

--- crates/dillhaven-assistant-text-to-speech/src/piper.rs
+++ crates/dillhaven-assistant-text-to-speech/src/piper.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use piper_rs::synth::PiperSpeechSynthesizer;
-use piper_rs::{PiperAudioResult, from_config_path};
+use piper_rs::{from_config_path, PiperAudioResult};
 use std::path::Path;
 
 pub const MODEL_PATH: &str = "/home/dylan/Documents/git/dillhaven-assistant/piper-finetune/data/en_US-patrickstewartemote-medium.onnx.json";
